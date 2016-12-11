05dceee7ef6ce58e69981bad75e75bc44bb0cec2
Minxian Liu <Minxian Liu>
2016-12-11 10:10:21 +0800

    refactor

---
+++ keeper/src/main/java/lmx/xml/family/keeper/commons/utils/RegexUtil.java
@@ -0,0 +1,7 @@
+package lmx.xml.family.keeper.commons.utils;
+
+/**
+ * Created by Aniwhel on 2016/12/11.
+ */
+public class RegexUtil {
+}
--- keeper/src/main/java/lmx/xml/keeper/commons/utils/RegexUtil.java
+++
@@ -1,7 +0,0 @@
-package lmx.xml.keeper.commons.utils;
-
-/**
- * Created by Aniwhel on 2016/12/10.
- */
-public class RegexUtil {
-}
